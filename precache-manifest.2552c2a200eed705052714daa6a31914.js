86204f0f1004f4e7588245d6765b9d3b181397c6
Akshay <akshaysinghal181@gmail.com>
2020-08-16 12:28:59 +0530

    Updates

---
+++ precache-manifest.2552c2a200eed705052714daa6a31914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e162ccc2c226ea81f91b7936314ab7be",
+    "url": "/Youtube-video-library/index.html"
+  },
+  {
+    "revision": "61726be266d89308b1dd",
+    "url": "/Youtube-video-library/static/css/main.5b1a55fd.chunk.css"
+  },
+  {
+    "revision": "7eae01a2487902147ec1",
+    "url": "/Youtube-video-library/static/js/2.1c510e38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Youtube-video-library/static/js/2.1c510e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61726be266d89308b1dd",
+    "url": "/Youtube-video-library/static/js/main.0e73333c.chunk.js"
+  },
+  {
+    "revision": "c3ecd7ca6be6af63bc0e",
+    "url": "/Youtube-video-library/static/js/runtime-main.b3bf147c.js"
+  }
+]);
